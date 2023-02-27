8e4f16afcfd0c2caddef6fb3fae3a87c30951898
MinGyu Kim <mingyugim584@gmail.com>
2023-02-27 16:37:19 +0900

    Initial commit

---
+++ photoalbum/src/main/java/com/squarecross/photoalbum/PhotoalbumApplication.java
@@ -0,0 +1,13 @@
+package com.squarecross.photoalbum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoalbumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoalbumApplication.class, args);
+	}
+
+}
---
+++ photoalbum/src/test/java/com/squarecross/photoalbum/PhotoalbumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squarecross.photoalbum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoalbumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
