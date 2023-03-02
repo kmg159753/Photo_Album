886cc6d1db8b094b0f0b4af3ae02d0b226f14bbc
MinGyu Kim <mingyugim584@gmail.com>
2023-03-02 19:49:03 +0900

    Update AlbumController.java

--- photoalbum/src/main/java/com/squarecross/photoalbum/controller/AlbumController.java
+++ photoalbum/src/main/java/com/squarecross/photoalbum/controller/AlbumController.java
@@ -5,10 +5,7 @@ import com.squarecross.photoalbum.service.AlbumService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/albums")
@@ -18,10 +15,28 @@ public class AlbumController {
 
     @RequestMapping(value = "/{albumId}", method = RequestMethod.GET)// Url 경로와 메서드 정의
 
-    public ResponseEntity<AlbumDto> getAlbum(@PathVariable("albumId") final long albumId){ // 메서드의 입출력 정의
+    public ResponseEntity<AlbumDto> getAlbum(@PathVariable("albumId") final long albumId) { // 메서드의 입출력 정의
 
         AlbumDto album = albumService.getAlbum(albumId);
         return new ResponseEntity<>(album, HttpStatus.OK);
 
     }
+
+    @RequestMapping(value = "/query", method = RequestMethod.GET)
+
+    ResponseEntity<AlbumDto> getAlbumByQuery(@RequestParam(value="albumId") final long albumId){
+        AlbumDto album = albumService.getAlbum(albumId);
+        return new ResponseEntity<>(album, HttpStatus.OK);
+    }
+
+
+    @RequestMapping(value = "/json_body",method = RequestMethod.POST)
+
+    ResponseEntity<AlbumDto> getAlbumByJson(@RequestBody final AlbumDto albumDto){
+        AlbumDto album = albumService.getAlbum(albumDto.getAlbumId());
+        return new ResponseEntity<>(album, HttpStatus.OK);
+    }
 }
+
+
+
