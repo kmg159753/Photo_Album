8c925ec391d5a05f3b1063bf4849354ca0dff620
kmg159753 <mingyugim584@gmail.com>
2023-05-26 22:22:55 +0900

    Update User.java
    Create column

--- photoalbum/src/main/java/com/squarecross/photoalbum/domain/User.java
+++ photoalbum/src/main/java/com/squarecross/photoalbum/domain/User.java
@@ -17,6 +17,9 @@ public class User {
     @Column(name = "user_id", unique = true, nullable = false)
     private Long userId;
 
+//    @Column(name = "user_password", unique = false, nullable = false)
+//    private Long userPassword;
+
     @Column(name = "user_name", unique = false, nullable = false)
     private String userName;
 
@@ -32,6 +35,55 @@ public class User {
     private Date loginAt;
 
 
-    @OneToMany(fetch = FetchType.LAZY,mappedBy = "album",cascade = CascadeType.ALL)
-    private List<Photo> photos;
+    @OneToMany(fetch = FetchType.LAZY,mappedBy = "user",cascade = CascadeType.ALL)
+    private List<Album> albums;
+
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getLoginAt() {
+        return loginAt;
+    }
+
+    public void setLoginAt(Date loginAt) {
+        this.loginAt = loginAt;
+    }
+
+    public List<Album> getAlbums() {
+        return albums;
+    }
+
+    public void setAlbums(List<Album> albums) {
+        this.albums = albums;
+    }
 }
