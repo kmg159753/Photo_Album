0fb4ea0e19e7bec282ac88f6253c6e0640949f88
kmg159753 <mingyugim584@gmail.com>
2023-05-27 22:37:14 +0900

    Update UserMapper.java

--- photoalbum/src/main/java/com/squarecross/photoalbum/mapper/UserMapper.java
+++ photoalbum/src/main/java/com/squarecross/photoalbum/mapper/UserMapper.java
@@ -1,13 +1,25 @@
 package com.squarecross.photoalbum.mapper;
 
 
+import com.squarecross.photoalbum.domain.Album;
 import com.squarecross.photoalbum.domain.User;
 
+import com.squarecross.photoalbum.dto.AlbumDto;
 import com.squarecross.photoalbum.dto.UserDto;
+
+import com.squarecross.photoalbum.mapper.AlbumMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class UserMapper {
 
+
+
     public static UserDto convertToDto(User user){
+
+        AlbumMapper albumMapper = new AlbumMapper();
 
         UserDto userDto = new UserDto();
         userDto.setUserId(user.getUserId());
@@ -16,6 +28,15 @@ public class UserMapper {
         userDto.setCreatedAt(user.getCreatedAt());
         userDto.setLoginAt(user.getLoginAt());
 
+        List<AlbumDto> albumDTOs = new ArrayList<>();
+
+        if (user.getAlbums() != null) {
+            for (Album album : user.getAlbums()) {
+                albumDTOs.add(albumMapper.convertToDto(album));
+            }
+        }
+
+        userDto.setAlbums(albumDTOs);
 
         return userDto;
     }
